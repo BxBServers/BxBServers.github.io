c2384ee9333ec64dbb1a91a51916d598b8610fcf
Griffin T <griffin_git@thommo.io>
2024-11-03 11:34:38 +0800

    render server pages from templates

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+
+        nativeBuildInputs = with pkgs; [just minijinja];
+        buildInputs = with pkgs; [];
+      in
+        with pkgs; {
+          formatter = alejandra;
+
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+    );
+}
